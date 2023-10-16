7ec1cab75bf76e2eff3c4f19bfc02695058543e9
stnganyani <85542837+coolhands-stn@users.noreply.github.com>
2023-10-16 16:25:43 +0200

    required updates

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0" async />
+        <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/toxicity" async />
+      </Head>
       <body>
         <Main />
         <NextScript />
